24d8afaa180034c4b8f032c1653148e73d5c766c
jbok <jbok@student.32seoul.kr>
2024-03-13 19:53:17 +0900

    [feat] Add necessary header files for networking

--- include/Ft_Irc.hpp
+++ include/Ft_Irc.hpp
@@ -3,5 +3,21 @@
 #define FT_IRC_HPP
 
 #include <iostream>
+#include <stdexcept>
+
+#include <cstring>
+#include <cerrno>
+#include <cstdio>
+
+#include <unistd.h>
+#include <poll.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 
 #endif
