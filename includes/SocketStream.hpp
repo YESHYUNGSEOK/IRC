8157a4c0c1594419be838d17b229b2b5d6698ce7
bok000111 <bok000111@naver.com>
2024-03-22 14:40:59 +0900

    [feat] Add exception classes and read vector of strings

--- includes/SocketStream.hpp
+++ includes/SocketStream.hpp
@@ -2,27 +2,30 @@
 #ifndef SOCKETSTREAM_HPP
 #define SOCKETSTREAM_HPP
 
+#include <exception>
 #include <string>
+#include <vector>
 
 #include "ft_irc.hpp"
 
 class SocketStream {
  private:
   static const int BUFFER_SIZE = 1024;
+  static const int LINE_SIZE_MAX = 512;
 
   const struct sockaddr_in _addr;
   const socklen_t _addr_len;
   const int _fd;
   std::string _read_buffer;
   std::string _write_buffer;
-  char *_raw_read_buffer;
-  char *_raw_write_buffer;
+  char *_raw_buffer;
 
- public:
   SocketStream();
-  SocketStream(const int server_fd);
   SocketStream(const SocketStream &src);
   SocketStream &operator=(const SocketStream &src);
+
+ public:
+  SocketStream(const int server_fd);
   ~SocketStream();
 
   int get_fd() const;
@@ -31,8 +34,42 @@ class SocketStream {
 
   SocketStream &operator<<(const std::string &data);
   SocketStream &operator>>(std::string &data);
+  SocketStream &operator>>(std::vector<std::string> &data);  // 안쓸수도 있음
 
-  void flush_write_buffer();
+  class SystemCallException : public std::exception {
+   public:
+    SystemCallException();
+    SystemCallException(const SystemCallException &src);
+    SystemCallException &operator=(const SystemCallException &src);
+    virtual ~SystemCallException() throw();
+    virtual const char *what() const throw();
+  };
+
+  class ConnectionClosedException : public std::exception {
+   public:
+    ConnectionClosedException();
+    ConnectionClosedException(const ConnectionClosedException &src);
+    ConnectionClosedException &operator=(const ConnectionClosedException &src);
+    virtual ~ConnectionClosedException() throw();
+    virtual const char *what() const throw();
+  };
+
+  class NoNewlineException : public std::exception {
+   public:
+    NoNewlineException();
+    NoNewlineException(const NoNewlineException &src);
+    NoNewlineException &operator=(const NoNewlineException &src);
+    virtual ~NoNewlineException() throw();
+    virtual const char *what() const throw();
+  };
+  class MessageTooLongException : public std::exception {
+   public:
+    MessageTooLongException();
+    MessageTooLongException(const MessageTooLongException &src);
+    MessageTooLongException &operator=(const MessageTooLongException &src);
+    virtual ~MessageTooLongException() throw();
+    virtual const char *what() const throw();
+  };
 };
 
 #endif  // IOHANDLER_HPP
