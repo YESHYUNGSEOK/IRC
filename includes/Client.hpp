fb1a62b6282c39c04b3099af46d3b4217f5e500f
bok000111 <bok000111@naver.com>
2024-03-22 14:41:12 +0900

    [feat] Refactor Client class and add new methods

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -2,19 +2,36 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include "Message.hpp"
 #include "SocketStream.hpp"
 #include "ft_irc.hpp"
 
 class Client {
  private:
-  // const int _fd;
-  // const struct sockaddr_in _client_addr;
-  // const socklen_t _client_addr_len;
+  bool _is_registered;
+  std::string _nickname;
+  std::string _username;
   SocketStream &_stream;
 
   Client();
   Client(const Client &src);
   Client &operator=(const Client &src);
+
+ public:
+  Client(int server_fd);
+  ~Client();
+
+  void set_register();
+  bool get_registraion() const;
+
+  void set_nickname(const std::string &nickname);
+  const std::string &get_nickname() const;
+  int get_fd() const;
+  std::string read_buffer();  // 연산자 오버로딩으로 변경해도 될 듯
+  void recv();
+  void send();
+
+  Message get_msg();
 
   bool operator==(const Client &other);
   bool operator!=(const Client &other);
@@ -22,18 +39,8 @@ class Client {
   bool operator>(const Client &other);
   bool operator<=(const Client &other);
   bool operator>=(const Client &other);
-
- public:
-  Client(int server_fd);
-  ~Client();
-
-  int get_fd() const;
-  std::string read_buffer();  // 연산자 오버로딩으로 변경해도 될 듯
-  void write_buffer(
-      const std::string &msg);  // 연산자 오버로딩으로 변경해도 될 듯
-  void recv();
-  void send();
-  void broadcast(const int origin_fd, const std::string &msg);
+  Client &operator<<(const std::string &data);
+  Client &operator>>(std::string &data);
 };
 
 #endif  // CLIENT_HPP
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,6 +1,7 @@
 #include "Client.hpp"
 
-Client::Client(int server_fd) : _stream(*new SocketStream(server_fd)) {
+Client::Client(int server_fd)
+    : _is_registered(false), _stream(*new SocketStream(server_fd)) {
   std::cout << "[Client] " << _stream.get_fd() << " connected" << std::endl;
 }
 
@@ -9,24 +10,49 @@ Client::~Client() {
   delete &_stream;
 }
 
+void Client::set_register() { _is_registered = true; }
+bool Client::get_registraion() const { return _is_registered; }
+void Client::set_nickname(const std::string &nickname) { _nickname = nickname; }
+const std::string &Client::get_nickname() const { return _nickname; }
+
 int Client::get_fd() const { return _stream.get_fd(); }
 
 std::string Client::read_buffer() {
   std::string msg;
   _stream >> msg;
+
   return msg;
 }
-
-void Client::write_buffer(const std::string &msg) { _stream << msg; }
 
 void Client::recv() { _stream.recv(); }
 
 void Client::send() { _stream.send(); }
 
-void Client::broadcast(const int origin_fd, const std::string &msg) {
-  // 클라이언트에게 메시지를 전송
-  _stream << "Broadcast from Client " << std::to_string(origin_fd) << ": "
-          << msg;
+Client::Client()  // 사용하지 않는 생성자
+    : _is_registered(false), _stream(*new SocketStream(0)) {
+  std::cout << "[Client] default constructer called - need to fix" << std::endl;
+}
+
+Client::Client(const Client &src)  // 사용하지 않는 복사 생성자
+    : _is_registered(src._is_registered),
+      _nickname(src._nickname),
+      _stream(*new SocketStream(0)) {
+  std::cout << "[Client] copy constructer called - need to fix" << std::endl;
+}
+
+Client &Client::operator=(const Client &src)  // 사용하지 않는 대입 연산자
+{
+  std::cout << "[Client] assign operater called - need to fix" << std::endl;
+  _is_registered = src._is_registered;
+  _nickname = src._nickname;
+  // _stream = *new SocketStream(0);
+  return *this;
+}
+
+Message Client::get_msg() {
+  std::string msg;
+  _stream >> msg;
+  return Message(msg);
 }
 
 bool Client::operator==(const Client &other) {
@@ -47,20 +73,13 @@ bool Client::operator<=(const Client &other) {
 bool Client::operator>=(const Client &other) {
   return _stream.get_fd() >= other._stream.get_fd();
 }
-
-// 사용하지 않는 생성자
-Client::Client() : _stream(*new SocketStream()) {
-  std::cout << "[Client] default constructer called - need to fix" << std::endl;
-}
-
-// 사용하지 않는 복사 생성자
-Client::Client(const Client &src) : _stream(*new SocketStream(src._stream)) {
-  std::cout << "[Client] copy constructer called - need to fix" << std::endl;
-}
-
-// 사용하지 않는 대입 연산자
-Client &Client::operator=(const Client &src) {
-  std::cout << "[Client] assign operater called - need to fix" << std::endl;
-  _stream = *new SocketStream(src._stream);
+Client &Client::operator<<(const std::string &data) {
+  _stream << data;
   return *this;
 }
+Client &Client::operator>>(std::string &data) {
+  _stream >> data;
+  return *this;
+}
+
+// Path: srcs/SocketStream.cpp
