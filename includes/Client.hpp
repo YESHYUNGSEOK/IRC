15d666bf1cd9c3d1da6e14fdbca60b695214ce45
jbok <jbok@student.32seoul.kr>
2024-03-25 21:07:54 +0900

    [feat] Add status flags and functions for client registration and negotiation

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -2,15 +2,39 @@
 #ifndef CLIENT_HPP
 #define CLIENT_HPP
 
+#include <vector>
+
 #include "Message.hpp"
 #include "SocketStream.hpp"
 #include "ft_irc.hpp"
 
+#define IS_CAP_NEGOTIATED(_status) ((_status & 0x01) == 0x01)
+#define IS_IN_NEGOTIATION(_status) ((_status & 0x02) == 0x02)
+#define IS_PASS_CONFIRMED(_status) ((_status & 0x04) == 0x04)
+#define IS_NICK_SET(_status) ((_status & 0x08) == 0x08)
+#define IS_USER_SET(_status) ((_status & 0x10) == 0x10)
+#define IS_REGISTERED(_status) ((_status & 0x1E) == 0x1E)
+
+#define SET_CAP_NEGOTIATED(_status) (_status |= 0x01)
+#define SET_IN_NEGOTIATION(_status) (_status |= 0x02)
+#define SET_PASS_CONFIRMED(_status) (_status |= 0x04)
+#define SET_NICK_SET(_status) (_status |= 0x08)
+#define SET_USER_SET(_status) (_status |= 0x10)
+#define SET_REGISTERED(_status) (_status |= 0x1E)
+
+#define UNSET_CAP_NEGOTIATED(_status) (_status &= ~0x01)
+#define UNSET_IN_NEGOTIATION(_status) (_status &= ~0x02)
+#define UNSET_PASS_CONFIRMED(_status) (_status &= ~0x04)
+#define UNSET_NICK_SET(_status) (_status &= ~0x08)
+#define UNSET_USER_SET(_status) (_status &= ~0x10)
+#define UNSET_REGISTERED(_status) (_status &= ~0x1E)
+
 class Client {
  private:
-  bool _is_registered;
+  unsigned int _status;
   std::string _nickname;
   std::string _username;
+  std::string _realname;
   SocketStream &_stream;
 
   Client();
@@ -21,18 +45,28 @@ class Client {
   Client(int server_fd);
   ~Client();
 
-  void set_register();
-  bool get_registraion() const;
-
-  void set_nickname(const std::string &nickname);
-  const std::string &get_nickname() const;
-  int get_fd() const;
+  // 시스템콜 관련 함수
   std::string read_buffer();  // 연산자 오버로딩으로 변경해도 될 듯
   void recv();
   void send();
 
+  // 기능 함수
   Message get_msg();
+  void start_negotiation();
+  void finish_negotiation();
+  void confirm_password();
+  void set_user(const std::string &username, const std::string &hostname,
+                const std::string &servername, const std::string &realname);
+  void register_client();
 
+  // getter, setter
+  unsigned int get_status() const;  // 가능하면 private으로 변경하는게 좋을 듯
+  bool is_registered() const;
+  void set_nickname(const std::string &nickname);
+  const std::string &get_nickname() const;
+  int get_fd() const;
+
+  // 연산자 오버로딩
   bool operator==(const Client &other);
   bool operator!=(const Client &other);
   bool operator<(const Client &other);
@@ -41,6 +75,7 @@ class Client {
   bool operator>=(const Client &other);
   Client &operator<<(const std::string &data);
   Client &operator>>(std::string &data);
+  Client &operator>>(std::vector<std::string> &vec);
 };
 
 #endif  // CLIENT_HPP
--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,21 +1,13 @@
 #include "Client.hpp"
 
 Client::Client(int server_fd)
-    : _is_registered(false), _stream(*new SocketStream(server_fd)) {
+    : _status(0), _stream(*new SocketStream(server_fd)) {
   std::cout << "[Client] " << _stream.get_fd() << " connected" << std::endl;
 }
-
 Client::~Client() {
   std::cout << "[Client] " << _stream.get_fd() << " disconnected" << std::endl;
   delete &_stream;
 }
-
-void Client::set_register() { _is_registered = true; }
-bool Client::get_registraion() const { return _is_registered; }
-void Client::set_nickname(const std::string &nickname) { _nickname = nickname; }
-const std::string &Client::get_nickname() const { return _nickname; }
-
-int Client::get_fd() const { return _stream.get_fd(); }
 
 std::string Client::read_buffer() {
   std::string msg;
@@ -23,37 +15,72 @@ std::string Client::read_buffer() {
 
   return msg;
 }
-
 void Client::recv() { _stream.recv(); }
-
 void Client::send() { _stream.send(); }
-
-Client::Client()  // 사용하지 않는 생성자
-    : _is_registered(false), _stream(*new SocketStream(0)) {
-  std::cout << "[Client] default constructer called - need to fix" << std::endl;
-}
-
-Client::Client(const Client &src)  // 사용하지 않는 복사 생성자
-    : _is_registered(src._is_registered),
-      _nickname(src._nickname),
-      _stream(*new SocketStream(0)) {
-  std::cout << "[Client] copy constructer called - need to fix" << std::endl;
-}
-
-Client &Client::operator=(const Client &src)  // 사용하지 않는 대입 연산자
-{
-  std::cout << "[Client] assign operater called - need to fix" << std::endl;
-  _is_registered = src._is_registered;
-  _nickname = src._nickname;
-  // _stream = *new SocketStream(0);
-  return *this;
-}
 
 Message Client::get_msg() {
   std::string msg;
   _stream >> msg;
   return Message(msg);
 }
+void Client::start_negotiation() {
+  if (IS_CAP_NEGOTIATED(_status) || IS_REGISTERED(_status)) return;
+  SET_IN_NEGOTIATION(_status);
+  std::cout << "[Client] " << _stream.get_fd() << " start negotiation"
+            << std::endl;
+}
+void Client::finish_negotiation() {
+  if (IS_REGISTERED(_status) || (!IS_IN_NEGOTIATION(_status))) return;
+  UNSET_IN_NEGOTIATION(_status);
+  SET_CAP_NEGOTIATED(_status);
+  std::cout << "[Client] " << _stream.get_fd() << " finish negotiation"
+            << std::endl;
+  register_client();
+}
+void Client::confirm_password() {
+  SET_PASS_CONFIRMED(_status);
+  std::cout << "[Client] " << _stream.get_fd() << " confirm password"
+            << std::endl;
+  register_client();
+}
+void Client::set_user(const std::string &username,
+                      __unused const std::string &hostname,
+                      __unused const std::string &servername,
+                      const std::string &realname) {
+  if ((!IS_PASS_CONFIRMED(_status))) return;
+  _username = username;
+  _realname = realname;
+  SET_USER_SET(_status);
+  std::cout << "[Client] " << _stream.get_fd() << " set user" << std::endl;
+  register_client();
+}
+void Client::register_client() {
+  if (IS_REGISTERED(_status) || (!IS_PASS_CONFIRMED(_status)) ||
+      (!IS_NICK_SET(_status)) || (!IS_USER_SET(_status)) ||
+      IS_IN_NEGOTIATION(_status)) {
+    std::cout << "[Client] " << _stream.get_fd() << " not registered"
+              << std::endl;
+    std::cout << "status: " << _status << std::endl;
+    return;
+  }
+  SET_REGISTERED(_status);
+  _stream << ":localhost 001 " << _nickname
+          << " :Welcome to the FT_IRC Network :" << _nickname << "!"
+          << _username << "@localhost"
+          << "\r\n";
+
+  std::cout << "[Client] " << _stream.get_fd() << " registered" << std::endl;
+}
+
+unsigned int Client::get_status() const { return _status; }
+bool Client::is_registered() const { return IS_REGISTERED(_status); }
+const std::string &Client::get_nickname() const { return _nickname; }
+void Client::set_nickname(const std::string &nickname) {
+  SET_NICK_SET(_status);
+  _nickname = nickname;
+  register_client();
+}
+int Client::get_fd() const { return _stream.get_fd(); }
 
 bool Client::operator==(const Client &other) {
   return _stream.get_fd() == other._stream.get_fd();
@@ -79,6 +106,40 @@ Client &Client::operator<<(const std::string &data) {
 }
 Client &Client::operator>>(std::string &data) {
   _stream >> data;
+  return *this;
+}
+Client &Client::operator>>(std::vector<std::string> &vec) {
+  while (true) {
+    try {
+      std::string msg = read_buffer();
+
+      vec.push_back(msg);
+    } catch (SocketStream::NoNewlineException &e) {
+      break;
+    } catch (SocketStream::MessageTooLongException &e) {
+      // *this << ERR_MSGTOOLONG_STR;
+    }
+  }
+
+  return *this;
+}
+
+Client::Client()  // 사용하지 않는 생성자
+    : _status(0), _stream(*new SocketStream(0)) {
+  std::cout << "[Client] default constructer called - need to fix" << std::endl;
+}
+
+Client::Client(const Client &src)  // 사용하지 않는 복사 생성자
+    : _status(0), _nickname(src._nickname), _stream(*new SocketStream(0)) {
+  std::cout << "[Client] copy constructer called - need to fix" << std::endl;
+}
+
+Client &Client::operator=(const Client &src)  // 사용하지 않는 대입 연산자
+{
+  std::cout << "[Client] assign operater called - need to fix" << std::endl;
+  _status = src._status;
+  _nickname = src._nickname;
+  // _stream = *new SocketStream(0);
   return *this;
 }
 
