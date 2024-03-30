192d0b18eb8cede4715c6053898d567381d6de50
bok000111 <bok000111@naver.com>
2024-03-30 20:27:43 +0900

    [FEAT] Refactor Server class: Remove unnecessary includes and add new member variables and functions

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -3,9 +3,6 @@
 #define SERVER_HPP
 
 #include <ctime>
-#include <deque>
-#include <iostream>
-#include <list>
 #include <map>
 #include <set>
 #include <sstream>
@@ -21,7 +18,6 @@
 class Client;
 class Channel;
 
-
 class Server {
  private:
   const int _port;
@@ -32,28 +28,31 @@ class Server {
   fd_set _read_fds;
   struct sockaddr_in _addr;
 
-	std::set<Client *> _clients;
-	std::set<Channel *> _channels;
+  std::set<Client *> _clients;
+  std::map<std::string, Client *> _clients_by_nick;
+  std::set<Channel *> _channels;
+  std::map<std::string, Channel *> _channels_by_name;
 
   // 사용 X
   Server();
   Server(__unused const Server &src);
   Server &operator=(__unused const Server &src);
 
-	// private utils
-	std::vector<std::string> split_tokens(const std::string &msg,
-										  const char delim);
-	bool check_channel_name(const std::vector<std::string> &channel_tokens) const;
-	Channel *get_target_channel(const std::string &channel_name) const;
-
-	void accept_new_client();
-	void read_client(Client *client);
-	void write_client(Client *client);
+  // private utils
+  void accept_new_client();
+  // std::size_t remove_client(Client *client);
+  std::set<Client *>::iterator remove_client(
+      std::set<Client *>::iterator client_it);
+  void read_client(Client *client);
+  void write_client(Client *client);
 
   // private command handlers
   void register_client(Client *client);
-  bool is_nick_in_use(const std::string &nickname);
-  int is_valid_option(const std::string &option);
+  void update_client_nick(Client *client, const std::string &new_nick);
+
+  // private utils
+  Client *find_client_by_nick(const std::string &nickname);
+  Channel *find_channel_by_name(const std::string &channel_name);
 
   // 명령어 처리
   void CAP(Client *client, const std::vector<std::string> &params);
@@ -63,17 +62,21 @@ class Server {
   void PING(Client *client, const std::vector<std::string> &params);
   void PONG(Client *client, const std::vector<std::string> &params);
 
-	// JOIN 명령어 처리
-	void join_channel(Client *client, std::string msg);
+  void JOIN(Client *client, const std::vector<std::string> &params);
+  void PART(Client *client, const std::vector<std::string> &params);
+  void TOPIC(Client *client, const std::vector<std::string> &params);
+  void INVITE(Client *client, const std::vector<std::string> &params);
 
-	// MODE 명령어 처리: ongoing...
-	void MODE(Client *client, const std::vector<std::string> &params);
+  // MODE 명령어 처리: ongoing...
+  void MODE(Client *client, const std::vector<std::string> &params);
 
-public:
-	Server(int port, std::string password);
-	~Server();
+ public:
+  Server(int port, std::string password);
+  ~Server();
 
-	void run();
+  void run();
+
+  Server &operator<<(const std::string &message);
 };
 
 #endif
