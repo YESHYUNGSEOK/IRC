26a7c0fc422f4574d0394380753b944d7e7ad3e5
hyungseok <eric.nohhyungseok@gmail.com>
2024-04-04 15:41:56 +0900

    [fix] fix mode

--- srcs/Message.cpp
+++ srcs/Message.cpp
@@ -1,45 +1,55 @@
 #include "Message.hpp"
 
 const std::string Message::_command_arr[22] = {
-    "NONE", "CAP",   "PASS",   "NICK",  "USER", "OPER",   "QUIT", "JOIN",
-    "PART", "TOPIC", "MODE",   "NAMES", "LIST", "INVITE", "KICK", "PRIVMSG",
-    "WHO",  "WHOIS", "WHOWAS", "KILL",  "PING", "PONG"};
+    "NONE", "CAP", "PASS", "NICK", "USER", "OPER", "QUIT", "JOIN",
+    "PART", "TOPIC", "MODE", "NAMES", "LIST", "INVITE", "KICK", "PRIVMSG",
+    "WHO", "WHOIS", "WHOWAS", "KILL", "PING", "PONG"};
 
 Message::Message(std::string &line)
-    : _command(NONE), _params(std::vector<std::string>()) {
+    : _command(NONE), _params(std::vector<std::string>())
+{
   const std::string::size_type clrf_pos = line.length() - 2;
   std::string::size_type pos = 0;
   std::string cmd;
 
   // 커맨드 추출 - 알파벳, 숫자로 이루어진 문자열만 추출
-  for (; pos < line.size(); pos++) {
-    if (!isalnum(line[pos])) break;
+  for (; pos < line.size(); pos++)
+  {
+    if (!isalnum(line[pos]))
+      break;
     cmd += toupper(line[pos]);
   }
   pos = cmd.size();
 
   // 커맨드 종류 판별
-  for (int i = 1; i < 22; i++) {
-    if (cmd == _command_arr[i]) {
+  for (int i = 1; i < 22; i++)
+  {
+    if (cmd == _command_arr[i])
+    {
       _command = static_cast<e_cmd>(i);
       break;
     }
   }
 
   // 커맨드가 잘못됐거나, 뒤에 공백이 아닌 문자가 있을 경우
-  if (_command == NONE || (pos != clrf_pos && line[pos] != ' ')) {
+  if (_command == NONE || (pos != clrf_pos && line[pos] != ' '))
+  {
     _params.push_back(cmd);
     return;
   }
 
-  while (pos <= clrf_pos) {
+  while (pos <= clrf_pos)
+  {
     pos = line.find_first_not_of(" ", pos);
-    if (pos == clrf_pos) break;
+    if (pos == clrf_pos)
+      break;
 
     std::string::size_type param_end = line.find_first_of("\r\n ", pos);
-    if (line[pos] == ':') {
+    if (line[pos] == ':')
+    {
       std::string::size_type param_end = line.find_first_of("\r\n", pos);
-      if (param_end != clrf_pos) {
+      if (param_end != clrf_pos)
+      {
         _command = NONE;
         return;
       }
@@ -53,7 +63,8 @@ Message::Message(std::string &line)
 
 Message::Message(const Message &src)
     : _command(src._command), _params(src._params) {}
-Message &Message::operator=(const Message &other) {
+Message &Message::operator=(const Message &other)
+{
   this->_command = other._command;
   this->_params = other._params;
   return *this;
@@ -61,25 +72,31 @@ Message &Message::operator=(const Message &other) {
 Message::~Message() {}
 
 Message::e_cmd Message::get_command() const { return this->_command; }
-const std::vector<std::string> &Message::get_params() const {
+const std::vector<std::string> &Message::get_params() const
+{
   return this->_params;
 }
 
 std::vector<std::string> Message::split_tokens(const std::string &str,
-                                               char delim) {
+                                               char delim)
+{
   std::vector<std::string> tokens;
   std::string token;
   std::istringstream tokenStream(str);
-  while (std::getline(tokenStream, token, delim)) {
+  while (std::getline(tokenStream, token, delim))
+  {
     tokens.push_back(token);
   }
   return tokens;
 }
 
 // gpt로 생성된 함수라 검증이 필요함
-bool Message::is_valid_nick(const std::string &nick) {
-  if (nick.size() > NICKLEN) return false;
-  for (std::string::const_iterator it = nick.begin(); it != nick.end(); it++) {
+bool Message::is_valid_nick(const std::string &nick)
+{
+  if (nick.size() > NICKLEN)
+    return false;
+  for (std::string::const_iterator it = nick.begin(); it != nick.end(); it++)
+  {
     if (!isalnum(*it) && *it != '-' && *it != '[' && *it != ']' &&
         *it != '\\' && *it != '_')
       return false;
@@ -87,13 +104,17 @@ bool Message::is_valid_nick(const std::string &nick) {
   return true;
 }
 
-bool Message::is_valid_channel_name(const std::string &name) {
-  if (name.size() < 2 || name.size() > CHANNELLEN) return false;
+bool Message::is_valid_channel_name(const std::string &name)
+{
+  if (name.size() < 2 || name.size() > CHANNELLEN)
+    return false;
 
   std::string::const_iterator it = name.begin();
-  if (*it != '#' && *it != '&') return false;
+  if (*it != '#' && *it != '&')
+    return false;
   it++;
-  for (; it != name.end(); it++) {
+  for (; it != name.end(); it++)
+  {
     if (!isalnum(*it) && *it != '-' && *it != '.' && *it != '-' && *it != '_')
       return false;
   }
@@ -101,17 +122,21 @@ bool Message::is_valid_channel_name(const std::string &name) {
   return true;
 }
 
-int Message::is_valid_mode_flag(const std::string &flag) {
-  if (flag.size() != 2 || flag[0] != '+' || flag[0] != '-') return -1;
-  if (flag == "i")
+int Message::is_valid_mode_flag(const std::string &flag)
+{
+  if (flag.size() != 2 || (flag[0] != '+' && flag[0] != '-'))
+  {
+    return -1;
+  }
+  if (flag[1] == 'i')
     return 0;
-  else if (flag == "t")
+  else if (flag[1] == 't')
     return 1;
-  else if (flag == "k")
+  else if (flag[1] == 'k')
     return 2;
-  else if (flag == "o")
+  else if (flag[1] == 'o')
     return 3;
-  else if (flag == "l")
+  else if (flag[1] == 'l')
     return 4;
   return -1;
 }
